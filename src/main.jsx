d6d601bc17f0ad4aa75488734cc64e328103acbc
Christina Lorenzo <christinalorenzo@gmail.com>
2024-02-13 13:44:40 +0100

    configurate react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom"
 import './index.css'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
@@ -13,12 +14,14 @@ import './index.css'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { CallBackHook } from './06-memos/CallBackHook'
 // import { Padre } from './07-tarea-memo/Padre'
-
 // import './08-useReducer/intro-reducer'
-import { TodoApp } from './08-useReducer/TodoApp'
+// import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <TodoApp />
-  // </React.StrictMode>
-)
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <MainApp />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
+  )
