e7137e05ac3e8d7b811b9c3bd0a58802334e5f72
Christina Lorenzo <christinalorenzo@gmail.com>
2024-02-14 20:38:21 +0100

    Test custom hooks

--- tests/hooks/useCounter.test.js
+++ tests/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { useCounter } from "../../src/hooks/useCounter";
 
 describe('Pruebas en el useCounter', () => {
@@ -20,9 +20,47 @@ describe('Pruebas en el useCounter', () => {
     const { result } = renderHook( () => useCounter(100) );
     const { counter } = result.current;
 
-
-
     expect( counter ).toBe(100);
+  });
+
+  test('Debe aumentar el contador', () => {
+    
+    const { result } = renderHook( () => useCounter(100) );
+    const { counter, increment } = result.current;
+
+    act( () => {
+      increment();
+      increment(2);
+    })
+
+    expect( result.current.counter ).toBe(103);
+    
+  });
+
+  test('Debe disminuir el contador', () => {
+    
+    const { result } = renderHook( () => useCounter(100) );
+    const { counter, decrement } = result.current;
+
+    act( () => {
+      decrement();
+    })
+
+    expect( result.current.counter ).toBe(99);
+    
+  });
+
+  test('Debe resetar el contador', () => {
+    
+    const { result } = renderHook( () => useCounter(100) );
+    const { counter, reset } = result.current;
+
+    act( () => {
+      reset();
+    })
+
+    expect( result.current.counter ).toBe(100);
+    
   });
 
 });
